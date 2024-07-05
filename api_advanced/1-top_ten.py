bcd1bfc227c123b2bc4276806e3855f21307152d
olaniyiolufemi <o.olufemi@alustudent.com>
2024-07-05 14:58:25 +0200

    kk

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,19 +4,33 @@ import requests
 
 
 def top_ten(subreddit):
-
     headers = {'User-Agent': 'MyAPI/0.0.1'}
     subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
     response = requests.get(subreddit_url, headers=headers)
 
     if response.status_code == 200:
         json_data = response.json()
-        for i in range(10):
-            print(
-                json_data.get('data')
-                .get('children')[i]
-                .get('data')
-                .get('title')
-            )
+        
+        # Debugging: Print JSON keys
+        print(json_data.keys())
+
+        data = json_data.get('data')
+        if data is not None:
+            children = data.get('children')
+            if children is not None:
+                for i in range(min(10, len(children))):
+                    title = children[i].get('data').get('title')
+                    if title is not None:
+                        print(title)
+                    else:
+                        print("Title not found")
+            else:
+                print("No children found in JSON data")
+        else:
+            print("No data found in JSON response")
     else:
         print(None)
+
+
+# Example call to the function
+top_ten('python')
